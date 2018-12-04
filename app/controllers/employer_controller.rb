881eea715716d30972ea71fbe7b3ce1c5de4e08a
Alan Keegan <alan.c.keegan@gmail.com>
2018-12-04 14:36:04 +0100

    deleted extra controllers

--- app/controllers/employer_controller.rb
+++
@@ -1,2 +0,0 @@
-class EmployerController < ApplicationController
-end
--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,4 +1,15 @@
 class Employer < ApplicationRecord
   has_many :listings, dependent: :destroy
   has_many :users
+
+  validates :contact, presence: true
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :description, presence: true
+  validates :city, presence: true
+  validates :photo, presence: true
+  validates :address, presence: true
+
+
+
 end
