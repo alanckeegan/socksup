c10372f6928e5652c09f27a5fc8abe945f399c14
Marina Kjaer <marinaillumkjaer@gmail.com>
2018-12-04 14:11:32 +0100

    Search results page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,9 +1,6 @@
 class ListingsController < ApplicationController
   def index
     @listings = Listing.all
-    if params[:query]
-      @city = params[:query][:city]
-    end
-    @listings = @listings.where(city: @city.capitalize) unless @city == "" || @city == nil
+    raise
   end
 end
