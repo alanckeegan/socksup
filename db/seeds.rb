c3ad952b59912496881d724eb20968e1d3409600
Marina Kjaer <marinaillumkjaer@gmail.com>
2018-12-04 15:12:16 +0100

    job show page works

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,18 @@ Quiz.destroy_all
 Employer.destroy_all
 
 puts 'Creating employers...'
-linekers = Employer.create!(name: 'Linekers', city: 'Barcelona')
-smokehouse = Employer.create!(name: 'Smokehouse', city: 'Ibiza')
+linekers = Employer.create!(name: 'Linekers', city: 'Barcelona', description: 'Exciting club in the heart of aq busy city', address: 'Barrio Gotico', contact: 'linekers@socksup.co.uk', photo: 'https://images.unsplash.com/photo-1532452119098-a3650b3c46d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80')
+smokehouse = Employer.create!(name: 'Smokehouse',description: 'Famous restaurant located on beautiful island', address: '15 Carrer de Lepant',city: 'Ibiza', contact: 'smokehouse@socksup.co.uk', photo: 'https://images.unsplash.com/photo-1522244451342-a41bf8a13d73?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80')
 
 puts 'Creating quizzes....'
 linkersquiz = Quiz.create!()
 smokehousequiz = Quiz.create!()
 
 puts 'Creating listings...'
-Listing.create!(title: 'Bartender', employer_id: linekers, quiz_id: linkersquiz, photo: 'https://images.unsplash.com/photo-1515674213736-54d14b4285b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80')
-Listing.create!(title: 'Hostess', employer_id: linekers, quiz_id: linkersquiz, photo: 'https://images.unsplash.com/photo-1453614512568-c4024d13c247?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=01be54aef2c30ab337248a5bc1439602&auto=format&fit=crop&w=1789&q=80')
-Listing.create!(title: 'PR', employer_id: linekers, quiz_id: linkersquiz, photo: 'https://images.unsplash.com/photo-1525857332689-4b3110c2d3f3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=64092aab3ca44d0dd8f1314166dba348&auto=format&fit=crop&w=1256&q=80')
-Listing.create!(title: 'Bartender', employer_id: smokehouse, quiz_id: smokehousequiz, photo: 'https://images.unsplash.com/photo-1468072114808-903e572b8ead?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1332&q=80')
-Listing.create!(title: 'Waitress', employer_id: smokehouse, quiz_id: smokehousequiz, photo: 'https://images.unsplash.com/photo-1511421616335-5a9846f1afcb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1789&q=80')
-Listing.create!(title: 'Pot wash', employer_id: smokehouse, quiz_id: smokehousequiz, photo: 'https://images.unsplash.com/photo-1470114755716-3e1124c6c3bd?ixlib=rb-0.3.5&s=804af933165df2475beefbd57c0634db&auto=format&fit=crop&w=1588&q=80')
+Listing.create!(title: 'Bartender', employer_id: linekers.id, quiz_id: linkersquiz.id, photo: 'https://images.unsplash.com/photo-1515674213736-54d14b4285b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80')
+Listing.create!(title: 'Hostess', employer_id: linekers.id, quiz_id: linkersquiz.id, photo: 'https://images.unsplash.com/photo-1453614512568-c4024d13c247?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=01be54aef2c30ab337248a5bc1439602&auto=format&fit=crop&w=1789&q=80')
+Listing.create!(title: 'PR', employer_id: linekers.id, quiz_id: linkersquiz.id, photo: 'https://images.unsplash.com/photo-1525857332689-4b3110c2d3f3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=64092aab3ca44d0dd8f1314166dba348&auto=format&fit=crop&w=1256&q=80')
+Listing.create!(title: 'Bartender', employer_id: smokehouse.id, quiz_id: smokehousequiz.id, photo: 'https://images.unsplash.com/photo-1468072114808-903e572b8ead?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1332&q=80')
+Listing.create!(title: 'Waitress', employer_id: smokehouse.id, quiz_id: smokehousequiz.id, photo: 'https://images.unsplash.com/photo-1511421616335-5a9846f1afcb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1789&q=80')
+Listing.create!(title: 'Pot wash', employer_id: smokehouse.id, quiz_id: smokehousequiz.id, photo: 'https://images.unsplash.com/photo-1470114755716-3e1124c6c3bd?ixlib=rb-0.3.5&s=804af933165df2475beefbd57c0634db&auto=format&fit=crop&w=1588&q=80')
 puts 'Finished!'
