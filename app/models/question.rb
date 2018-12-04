1498eedf15581fad9aa58dab118275e9e8397c68
Alan Keegan <alan.c.keegan@gmail.com>
2018-12-04 15:23:39 +0100

    validaaaaaaaaaations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :quiz
   has_many :responses,  dependent: :destroy
+
+  validates :question_type, inclusion: { in: %w(multi field)}, :allow_nil => false
+  validates :question, presence: true
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,6 @@
 class Response < ApplicationRecord
   belongs_to :submission
   belongs_to :question
+
+  validates :content, presence: true
 end
