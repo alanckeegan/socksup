1310c356e04dd04bfb6631a03797c2409f8cbd7d
Lucas Rocha <ld.rocha@hotmail.com>
2018-12-05 16:09:13 +0100

    Unfinished quiz page

--- app/javascript/components/quiz.js
+++ app/javascript/components/quiz.js
@@ -1,4 +1,21 @@
-// 1. find a selector
-// 2. select the element
-// 3. add listener
-// 4. fire callback (adding a class)
+// use querySelector to get inline by class
+
+// addeventlistener, onclick to
+// removeclasslist active from all inline
+// addclasslist 'active' to the event.target
+
+// in css, make your active class to be the fill color
+const buttons = document.querySelectorAll('.collection_radio_buttons');
+
+function addListener(element) {
+  element.addEventListener('click', function(event) {
+    buttons.forEach(button => button.classList.remove("active"))
+    // console.log(event)
+    // event.removeClassList('active')
+    event.currentTarget.classList.toggle('active');
+  })
+}
+
+if (buttons) {
+  buttons.forEach(addListener);
+}
