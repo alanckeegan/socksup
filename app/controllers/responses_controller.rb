b34f390588dba5d8469f85bef4803dce0141e65a
Alan Keegan <alan.c.keegan@gmail.com>
2018-12-05 17:52:34 +0100

    ONE QUESTION AT A TIIIIIIIME

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -6,6 +6,8 @@ class ResponsesController < ApplicationController
     @response.submission = Submission.find(params[:submission_id])
     @response.content = params[:response][:content]
     @response.save
+
+    @next_question = @response.question.quiz.questions[@response.question.quiz.questions.index(@response.question)+1]
   end
 
 end
