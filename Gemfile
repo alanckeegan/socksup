7941ef6de933b2042dd3873cf6c1784ddcc2c2f8
Marina Kjaer <marinaillumkjaer@gmail.com>
2018-12-05 16:34:53 +0100

    results page form works

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'puma'
 gem 'rails', '5.2.1.1'
 gem 'redis'
 gem 'geocoder'
-gem 'pg_search'
+
 
 gem 'autoprefixer-rails'
 gem 'bootstrap-sass', '~> 3.3'
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,11 +2,21 @@ class ListingsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    if params[:query][:city] == ("" || nil)
-      @listings = Listing.all
+    # if params[:query][:city] == ("" || nil)
+    #   @listings = Listing.all
+    # else
+    #   @listings = Listing.joins(:employer).where(employers: {city: params[:query][:city].capitalize})
+    # end
+    @listings = Listing.all
+    if params[:query]
+      @city = params[:query][:city]
+      @title = params[:query][:title]
     else
-      @listings = Listing.joins(:employer).where(employers: {city: params[:query][:city].capitalize})
+      params[:query] = {}
     end
+
+    @listings = @listings.joins(:employer).where(employers: {city: @city.capitalize}) unless @city == "" || @city == nil
+    @listings = @listings.where("title ILIKE ?", "%#{@title}%") unless @title == "" || @title == nil
   end
 
   def show
