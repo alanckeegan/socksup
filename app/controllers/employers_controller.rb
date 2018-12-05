f166dc201f19b302c4ffd9c065d80e02244a0801
Marina Kjaer <marinaillumkjaer@gmail.com>
2018-12-05 12:00:37 +0100

    profile page variables available to design

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -1,5 +1,5 @@
 class EmployersController < ApplicationController
   def create
-    # make sure that the input of the city is capitalized when saved to the database so that the search comparision works!!!
+    # - make sure that the input of the city is capitalized when saved to the database so that the search comparision works!!!
   end
 end
