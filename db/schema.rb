ee4f62bb58e712ac2de8f42f8b79e5ab839ee60c
Marina Kjaer <marinaillumkjaer@gmail.com>
2018-12-10 10:18:09 +0100

    add message table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_07_112237) do
+ActiveRecord::Schema.define(version: 2018_12_10_085105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,16 @@ ActiveRecord::Schema.define(version: 2018_12_07_112237) do
     t.integer "pay_per_hour"
     t.index ["employer_id"], name: "index_listings_on_employer_id"
     t.index ["quiz_id"], name: "index_listings_on_quiz_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "submission_id_id"
+    t.bigint "user_id_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["submission_id_id"], name: "index_messages_on_submission_id_id"
+    t.index ["user_id_id"], name: "index_messages_on_user_id_id"
   end
 
   create_table "questions", force: :cascade do |t|
