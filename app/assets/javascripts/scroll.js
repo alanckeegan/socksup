d3653efea927849c4950081c65c15c41ccf0f65d
Alan Keegan <alan.c.keegan@gmail.com>
2018-12-11 12:15:37 +0100

    bug fixed

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,5 +1,8 @@
 function scrollLastMessageIntoView() {
-const messages = document.querySelectorAll('.message');
-const lastMessage = messages.[messages.last -1];
-lastMessage.scrollIntoView()
+  const messages = document.querySelectorAll('.message');
+  const lastMessage = messages[messages.length - 1];
+
+  if (lastMessage !== undefined) {
+    lastMessage.scrollIntoView();
+  }
 }
