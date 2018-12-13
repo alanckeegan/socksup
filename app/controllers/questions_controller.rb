f25bcf1703ca0350f8db16b904333cf3127d4813
Pierre Herve-Bazin <pierrehervebazin@gmail.com>
2018-12-13 16:15:15 +0100

    one commit message

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,30 @@
 class QuestionsController < ApplicationController
 
+  def show
+    @quiz = Listing.find(params[:listing_id]).quiz
+    @questions = Question.where(quiz_id: @quiz.id)
+    @listing = Listing.find(params[:listing_id])
+  end
+
+
   def new
     @question = Question.new
+    @listing = Listing.find(params[:listing_id])
+    @quiz = Listing.find(params[:listing_id]).quiz
+    @employer = Employer.find(params[:employer_id])
   end
 
   def create
     quiz = Listing.find(params[:listing_id]).quiz_id
+    @employer = Employer.find(params[:employer_id])
+    @listing = Listing.find(params[:listing_id])
     @question = Question.create(question: params[:question], correct_answer: params[:correct_answer], wrong_answers: [params[:wrong_answers0], params[:wrong_answers1], params[:wrong_answers2]], quiz_id: quiz, question_type: params[:question_type])
     @question.save!
-    redirect_to user_path(current_user)
+    if params[:finish_quiz]
+      redirect_to employer_listing_question_path(@employer, @listing, quiz)
+    else
+      redirect_to new_employer_listing_question_path(@employer, @listing)
+    end
   end
 
   private
--- app/javascript/components/display-mc-answer.js
+++ app/javascript/components/display-mc-answer.js
@@ -1,15 +1,21 @@
 const multipleChoicePopup = () => {
   const multipleCheckbox = document.getElementById('question_type_mc');
   const openAnswerCheckbox = document.getElementById('question_type_os');
+  const submitButton = document.getElementById('submit_button_question');
+
+    openAnswerCheckbox.addEventListener('click', function(){
+      document.getElementById('multiple_answers').style.visibility = 'hidden';
+      document.getElementById('multiple_answers').style.height = 0;
+      document.getElementById('submit_button_question').style.visibility = 'visible';
+      document.getElementById('submit_button_question_finish').style.visibility = 'visible';
+  });
 
   multipleCheckbox.addEventListener('click', function(event){
         document.getElementById('multiple_answers').style.visibility = 'visible';
+        document.getElementById('multiple_answers').style.height = '250px';
+        document.getElementById('submit_button_question').style.visibility = 'visible';
+        document.getElementById('submit_button_question_finish').style.visibility = 'visible';
   });
-
-  openAnswerCheckbox.addEventListener('click', function(){
-      document.getElementById('multiple_answers').style.visibility = 'hidden';
-  });
-
 }
 
 export { multipleChoicePopup };
